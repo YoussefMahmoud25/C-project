f47286275825d4cd401a365c0a5f62f6c82f2a22
Youssef Mahmoud <yousof9008@gmail.com>
2022-12-16 14:28:17 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "../Project1/Terminal/terminal.h"
+#include "../Project1/Application/app.h"
+
+void main()
+{
+	appStart();
+}
+/*isCardExpired(&customer1, &terminal1);
+		getTransactionAmount(&terminal1);
+		setMaxAmount(&terminal1, maxAmount);
+		isBelowMaxAmount(&terminal1);*/
